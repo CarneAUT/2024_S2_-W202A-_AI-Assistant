d935d31a27ddcbd76c4d0e66025ca60e95877920
Philip <philipjin4@gmail.com>
2024-08-30 10:59:13 +1200

    added ai

--- webpages/mock_quiz.py
+++ webpages/mock_quiz.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
 # Upload files
+
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama3-8b-8192",
+)
+
+print(chat_completion.choices[0].message.content)
