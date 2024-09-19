99c921104a82e53e7f402be9fcec691984e23ea3
Philip <philipjin4@gmail.com>
2024-09-20 11:25:15 +1200

    created AI generated text functionalityg

--- webpages/study_plan.py
+++ webpages/study_plan.py
@@ -1,5 +1,27 @@
 import streamlit as st
+from pypdf import PdfReader
+from generate_text import *
+from prompts import *
 
 # Upload files
-st.write("This is the Study Plan Page")
-st.write("anythin")
+st.header("Upload Files For The AI")
+st.write("You can upload either a pdf file below for the AI to look through and summarise for your convenience. It cannot read words on images such as png or jpeg.")
+
+# Create an uploader for files to be uploaded to.
+uploader = st.file_uploader(
+    "", accept_multiple_files=False, help="Only pdf files can be accepted.",
+    type=["pdf"]  # File types allowed
+)
+
+text = ""
+
+# When a file is uploaded.
+if uploader is not None:
+    # Check if the file is a PDF
+    if uploader.type == "application/pdf":
+        reader = PdfReader(uploader)
+
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+
+st.write(generate_text(text, study_plan))
