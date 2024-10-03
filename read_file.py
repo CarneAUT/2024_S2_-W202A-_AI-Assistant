f0a6dad72c0627ec2f0231686e3937daee44b1a5
Philip <165343362+bdkhi1@users.noreply.github.com>
2024-10-04 11:29:52 +1300

    Improve code readability
    Create new read_file.py for reading and extracting text.

---
+++ read_file.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from pypdf import PdfReader
+import docx
+
+def read_pdf(file):
+    text = ""
+    reader = PdfReader(file)
+
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+
+    return text
+
+def read_docx(file):
+    doc = docx.Document(file)
+    content = []
+    for para in doc.paragraphs:
+        content.append(para.text)
+    return '\n'.join(content)
+def extract_text(uploaded_file):
+    text = ""
+
+    if uploaded_file.type == "application/pdf":
+        text = read_pdf(uploaded_file)
+
+    elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        text = read_docx(uploaded_file)
+
+    return text
+
